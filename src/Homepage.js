4d317817596b876e4643004a195b2894d0d681bd
Jon Tsikniadopoulos <jon.tsiknias@gmail.com>
2021-02-06 23:08:50 -0800

    add api file

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,31 +1,9 @@
-import Axios from 'axios';
+import fetchArticles from './api';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
-function Homepage() {
-    Axios({
-        method: 'GET',
-        url:
-            'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI',
-        params: {
-            q: 'united states news',
-            pageNumber: '1',
-            pageSize: '10',
-            autoCorrect: 'true',
-            withThumbnails: 'true',
-            fromPublishedDate: 'null',
-            toPublishedDate: 'null',
-        },
-        headers: {
-            'Content-Type': 'application/json',
-            'x-rapidapi-key':
-                '9d3c2abe39msh77772478b03e4ecp1a2b09jsneb4c0b18252f',
-            'x-rapidapi-host':
-                'contextualwebsearch-websearch-v1.p.rapidapi.com',
-        },
-    }).then((res) => {
-        console.log(res.data.value);
-    });
+const Homepage = () => {
+    console.log(fetchArticles());
     return (
         <div className='App'>
             <header className='App-header'>
@@ -36,6 +14,6 @@ function Homepage() {
             </header>
         </div>
     );
-}
+};
 
 export default Homepage;
---
+++ src/api.js
@@ -0,0 +1,27 @@
+import Axios from 'axios';
+
+async function fetchArticles() {
+    const method = 'GET';
+    const url =
+        'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI';
+    const params = {
+        q: 'united states news',
+        pageNumber: '1',
+        pageSize: '10',
+        autoCorrect: 'true',
+        withThumbnails: 'true',
+        fromPublishedDate: 'null',
+        toPublishedDate: 'null',
+    };
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'x-rapidapi-key': '9d3c2abe39msh77772478b03e4ecp1a2b09jsneb4c0b18252f',
+        'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
+    };
+
+    const articles = await Axios({ method, url, params, headers });
+    return articles;
+}
+
+export default fetchArticles;
