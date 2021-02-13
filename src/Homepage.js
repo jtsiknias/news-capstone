4970512b073e0e6c5afcdf145f6eaa3baf104fed
Jon Tsikniadopoulos <jon.tsiknias@gmail.com>
2021-02-12 23:25:03 -0800

    add useEffect

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,26 +1,48 @@
-import fetchArticles from './api';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
-const Homepage = () => {
-    async function getArticles() {
-        const results = await fetchArticles();
-        console.log(results);
-        return results;
-    }
+function Homepage() {
+    const [data, setData] = useState({ hits: [] });
 
-    getArticles();
+    useEffect(() => {
+        const method = 'GET';
+        const url =
+            'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI';
+        const params = {
+            q: 'united states news',
+            pageNumber: '1',
+            pageSize: '10',
+            autoCorrect: 'true',
+            withThumbnails: 'true',
+            fromPublishedDate: 'null',
+            toPublishedDate: 'null',
+        };
 
+        const headers = {
+            'Content-Type': 'application/json',
+            'x-rapidapi-key':
+                '9d3c2abe39msh77772478b03e4ecp1a2b09jsneb4c0b18252f',
+            'x-rapidapi-host':
+                'contextualwebsearch-websearch-v1.p.rapidapi.com',
+        };
+        const fetchData = async () => {
+            const result = await Axios({ method, url, params, headers });
+            setData(result);
+        };
+        fetchData();
+    }, []);
     return (
         <div className='App'>
             <header className='App-header'>
                 <Container className='d-flex flex-column'>
                     <Button className='mb-3'>Home</Button>
-                    <Button>Test - Test</Button>
+                    <ul>{data.data.value.map((article) => ({ article }))}</ul>
                 </Container>
             </header>
         </div>
     );
-};
+}
 
 export default Homepage;
